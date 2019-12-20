5313aa521b6621cc7baa5fbc2e102e4141cc7265
Bogdan Grigoruta <bogdangrigoruta@gmail.com>
2019-12-20 17:25:03 +0200

    Delete BUILD dir

--- BUILD/src/utils/queue/queue.d
+++
@@ -1,5 +0,0 @@
-src/utils/queue/queue.o: \
- /home/boco/bosch/BFMC2020.Embedded.Platform/src/utils/queue/queue.cpp \
- /home/boco/bosch/BFMC2020.Embedded.Platform/mbed_config.h \
- /home/boco/bosch/BFMC2020.Embedded.Platform/include/utils/queue/queue.hpp \
- /home/boco/bosch/BFMC2020.Embedded.Platform/include/utils/queue/queue.tpp
